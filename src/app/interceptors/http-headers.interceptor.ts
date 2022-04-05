711dd011fc415295ae1d25c4383429346c73f5eb
zenyaboi <bannedboyinlife@gmail.com>
2022-04-05 15:03:45 -0300

    created http interceptors

---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': '',
+        'x-rapidapi-host': '',
+      },
+      setParams: {
+        key: '',
+      },
+    });
+    return next.handle(req);
+  }
+}
